3c8317e0a62481ced12d137402601008320f9ab8
Allan Gikonyo <gikonyo257@gmail.com>
2022-01-11 16:44:01 +0300

    began adding jquery elements

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// $(document).ready(function() {
+//     $("figure").click(function() {
+//       $("design").show();
+//     });
+//   });
+
+//   $(document).ready(function() {
+//     $("figure").click(function() {
+//       $("development").show();
+//     });
+//   });
+
+//   $(document).ready(function() {
+//     $("figure").click(function() {
+//       $("productmanagement").show();
+//     });
+//   });
+
+
+//   $(document).ready(function() {
+//     $("p").click(function() {
+//       $(".walrus-showing").toggle();
+//       $(".walrus-hidden").toggle();
+//     });
+//   });
+
+  $(document).ready(function() {
+    $('#image-vangogh').click(function(){
+
+      $(".design-icon-showing").toggle();
+      $(".design-hidden").toggle();
+
+    });
+  });
+
+// var icons = document.getElementsByTagName('figure');
+// for (var i = 0; i < icons.length; i++) {
+//   icons[i].addEventListener('click',function() {
+//     this.classList.toggle('active');
+//   });
+// }
+
+

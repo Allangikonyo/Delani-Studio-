515fe58ced0abd25133c3d3fc465752158acd7b1
Allan Gikonyo <gikonyo257@gmail.com>
2022-01-13 18:05:10 +0300

    made page responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,50 @@
-// $(document).ready(function() {
-//     $("figure").click(function() {
-//       $("design").show();
-//     });
-//   });
+$(document).ready(function() {
 
-//   $(document).ready(function() {
-//     $("figure").click(function() {
-//       $("development").show();
-//     });
-//   });
-
-//   $(document).ready(function() {
-//     $("figure").click(function() {
-//       $("productmanagement").show();
-//     });
-//   });
-
-
-//   $(document).ready(function() {
-//     $("p").click(function() {
-//       $(".walrus-showing").toggle();
-//       $(".walrus-hidden").toggle();
-//     });
-//   });
-
-  $(document).ready(function() {
-    $('#image-vangogh').click(function(){
-
+    $("#design").click(function(){
       $(".design-icon-showing").toggle();
       $(".design-hidden").toggle();
-
     });
-  });
 
-// var icons = document.getElementsByTagName('figure');
-// for (var i = 0; i < icons.length; i++) {
-//   icons[i].addEventListener('click',function() {
-//     this.classList.toggle('active');
-//   });
-// }
 
+    $("#designpar").click(function(){
+        $(".design-icon-showing").toggle();
+        $(".design-hidden").toggle();
+    });
+
+    $("#dev").click(function(){
+        $(".dev-icon-showing").toggle();
+        $(".dev-hidden").toggle();
+      });
+
+    $("#devpar").click(function(){
+        $(".dev-icon-showing").toggle();
+        $(".dev-hidden").toggle();
+      });
+
+    $("#product").click(function(){
+        $(".product-icon-showing").toggle();
+        $(".product-hidden").toggle();
+      });
+
+    $("#productpar").click(function(){
+        $(".product-icon-showing").toggle();
+        $(".product-hidden").toggle();
+      });
+
+});
+
+$(document).ready(function() {
+    
+    $("#submitForm").click(function(){
+        let Name = (document.getElementById("Name").value )
+        let  Email = (document.getElementById("Email").value )
+        let Message = (document.getElementById("Message").value )
+        // let key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+        if (Name == '' || Email == '' || Message == '') {
+            alert('Please fill out the required fields');
+        } else {
+            alert(' Hi ' + Name + '! ' + ' Thank you for contacting us. We shall respond promptly!');
+        }
+    });
+});
 
